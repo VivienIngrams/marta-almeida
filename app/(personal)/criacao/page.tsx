cb8984ef082b473d6cb84d61667a9500e19148e4
Vivien <vivingrams@gmail.com>
2025-07-02 17:13:21 +0100

    Criacao page functioning

--- app/(personal)/criacao/page.tsx
+++ app/(personal)/criacao/page.tsx
@@ -1,12 +1,14 @@
-import { loadCriacaoPage, loadProject } from '@/sanity/loader/loadQuery'
-
 import ProjectPreview from '@/components/pages/project/ProjectPreview'
+import { CustomPortableText } from '@/components/shared/CustomPortableText'
+import { loadCriacaoPage, loadProject } from '@/sanity/loader/loadQuery'
 
 export default async function CriacaoPage() {
   // Fetch the Criação page data (with showcaseProjects)
   const criacao = await loadCriacaoPage()
   const showcaseProjects = criacao?.data?.showcaseProjects || []
-
+  const title = criacao?.data?.title
+  const overview = criacao?.data?.overview
+console.log('CriacaoPage data:', title, overview)
   // Fetch initial data for each project
   const projectsWithInitial = await Promise.all(
     showcaseProjects.map(async (project: any) => {
@@ -15,12 +17,17 @@ export default async function CriacaoPage() {
         slug: project.slug,
         initial,
       }
-    })
+    }),
   )
 
   return (
     <section className="space-y-12 md:px-6 py-10">
-      <h1 className="text-4xl font-bold">Criação</h1>
+      <h1 className="text-4xl font-bold">{title}</h1>
+      {overview?.text && (
+        <div className="mt-2 text-lg md:text-xl 2xl:text-2xl text-right max-w-[80%] ml-auto cursor-pointer">
+          <CustomPortableText value={overview.text} />
+        </div>
+      )}
       <div className=" gap-8">
         {projectsWithInitial.map((project) => (
           <ProjectPreview
