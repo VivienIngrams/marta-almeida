7bfd8ca12492904f2fccaa9c9798ed979022cdeb
Vivien <vivingrams@gmail.com>
2025-09-30 14:36:29 +0100

    WIden text

--- components/pages/home/HomePage.tsx
+++ components/pages/home/HomePage.tsx
@@ -77,7 +77,7 @@ export function HomePage({ data, language }: HomePageProps) {
             )}
             {/* Overview text below the image */}
             {overviewText && (
-              <div className="mt-2 text-base md:text-lg 2xl:text-xl text-black text-right max-w-[80%] ml-auto cursor-pointer">
+              <div className="mt-2 text-base md:text-lg 2xl:text-xl text-black text-right max-w-[95%] ml-auto cursor-pointer">
                 <CustomPortableText value={overviewText} />
               </div>
             )}
