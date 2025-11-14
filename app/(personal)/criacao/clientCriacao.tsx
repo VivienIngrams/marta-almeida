eb5249a8bead87c55dc9c3d113770942713d83b5
Vivien <vivingrams@gmail.com>
2025-11-14 16:43:32 +0000

    Smaller titles

--- app/(personal)/criacao/clientCriacao.tsx
+++ app/(personal)/criacao/clientCriacao.tsx
@@ -45,7 +45,7 @@ useEffect(() => {
   
       <div className="lg:pl-[20%] pb-16 pt-28 lg:pt-16 min-h-screen">
         <div className="px-4 lg:pr-8 2xl:pr-24 mb-8">
-          <h1 className="hidden lg:block text-center uppercase text-3xl lg:text-5xl 2xl:text-7xl font-light tracking-tight">
+          <h1 className="hidden lg:block text-center uppercase text-3xl xl:text-4xl 2xl:text-5xl font-light tracking-tight">
             {title}
           </h1>
         </div>
