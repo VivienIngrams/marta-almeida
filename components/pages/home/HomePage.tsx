8c5e95a2b56737a46ba577b4a6c65afc043fe287
Vivien <vivingrams@gmail.com>
2025-11-14 17:41:18 +0000

    Padding top

--- components/pages/home/HomePage.tsx
+++ components/pages/home/HomePage.tsx
@@ -44,7 +44,7 @@ export function HomePage({ data, language }: HomePageProps) {
           : undefined,
       }}
     >
-      <div className="py-10  lg:px-6  lg:pr-8 lg:pt-16  px-4 lg:pl-80 2xl:pl-96">
+      <div className="py-10  lg:px-6  lg:pr-8 lg:pt-16 xl:pt-20  px-4 lg:pl-80 2xl:pl-96">
       <div
         className="
         flex flex-col items-center 
