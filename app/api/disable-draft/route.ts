e5edda94b24457ae03f79423dee1a4b26e15e544
Vivien <vivingrams@gmail.com>
2026-01-22 19:51:18 +0000

    api route file

--- app/api/disable-draft/route.ts
+++ app/api/disable-draft/route.ts
@@ -2,6 +2,12 @@ import { draftMode } from 'next/headers'
 import { NextRequest, NextResponse } from 'next/server'
 
 export function GET(request: NextRequest) {
+  const secret = request.nextUrl.searchParams.get('secret')
+
+  if (secret !== process.env.DRAFT_SECRET) {
+    return new NextResponse('Forbidden', { status: 403 })
+  }
+
   draftMode().disable()
   const url = new URL(request.nextUrl)
   return NextResponse.redirect(new URL('/', url.origin))
